304a62cbbf8d0dd523d5de1280b86fdabb0917e5
Shaped Sundew <16618209+Shapedsundew9@users.noreply.github.com>
2023-09-10 11:53:03 -0400

    Latest

--- gp_monitor.py
+++ gp_monitor.py
@@ -1,5 +1,6 @@
 import gi
-gi.require_version('Gtk', '3.0')
+
+gi.require_version("Gtk", "3.0")
 
 from matplotlib.pyplot import inferno
 from time import time
@@ -7,12 +8,32 @@ from pypgtable import table
 from logging import DEBUG, INFO, WARN, ERROR, FATAL, NullHandler, getLogger
 from graph_tool import Graph, Vertex
 from graph_tool.util import find_vertex
-from graph_tool.draw import graph_draw, interactive_window, sfdp_layout, GraphWindow, GraphWidget
+from graph_tool.draw import (
+    graph_draw,
+    interactive_window,
+    sfdp_layout,
+    GraphWindow,
+    GraphWidget,
+)
 from graph_tool.topology import label_components
 from matplotlib.cm import viridis, plasma, inferno, magma, cividis
-from matplotlib.cm import Greys as greys, Purples as purples, Blues as blues, Greens as greens, Oranges as oranges, Reds as reds
+from matplotlib.cm import (
+    Greys as greys,
+    Purples as purples,
+    Blues as blues,
+    Greens as greens,
+    Oranges as oranges,
+    Reds as reds,
+)
 from matplotlib.cm import cool, hot, gist_heat
-from matplotlib.cm import gnuplot, gnuplot2, CMRmap as cmrmap, rainbow, jet, nipy_spectral
+from matplotlib.cm import (
+    gnuplot,
+    gnuplot2,
+    CMRmap as cmrmap,
+    rainbow,
+    jet,
+    nipy_spectral,
+)
 from gi.repository import Gtk, Gdk, GdkPixbuf, GObject, GLib, Gio
 
 _logger = getLogger(__name__)
@@ -21,26 +42,18 @@ _logger.addHandler(NullHandler())
 
 # The gene pool default config
 # Tree structure
-_LEL = 'ancestor_a_ref'
-_REL = 'ancestor_b_ref'
-_NL = 'ref'
-_PTR_MAP = {
-    _LEL: _NL,
-    _REL: _NL
-}
+_LEL = "ancestor_a_ref"
+_REL = "ancestor_b_ref"
+_NL = "ref"
+_PTR_MAP = {_LEL: _NL, _REL: _NL}
 _DEFAULT_GP_CONFIG = {
-    'database': {
-        'dbname': 'test_db',
+    "database": {
+        "dbname": "test_db",
     },
-    'ptr_map': _PTR_MAP,
-    'table': 'gene_pool'
+    "ptr_map": _PTR_MAP,
+    "table": "gene_pool",
 }
-_COLUMNS = (
-    _NL,
-    _LEL,
-    _REL,
-    'survivability'
-)
+_COLUMNS = (_NL, _LEL, _REL, "survivability")
 
 _OVER_MAX = 1 << 64
 _MASK = _OVER_MAX - 1
@@ -48,8 +61,8 @@ ref_str = lambda x: f"{(_OVER_MAX + x) & _MASK:016x}"
 
 print("Loading Gene Pool...")
 gp = table(_DEFAULT_GP_CONFIG)
-gcs = tuple(gp.recursive_select('WHERE {population_uid}={pid}', literals={'pid': 1}))
-#for gc in gcs: print(gc)
+gcs = tuple(gp.recursive_select("WHERE {population_uid}={pid}", literals={"pid": 1}))
+# for gc in gcs: print(gc)
 
 print(f"Creating population of {len(gcs)} GC's in graph...")
 g = Graph()
@@ -57,7 +70,7 @@ vertices = g.add_vertex(len(gcs))
 pin = g.new_vertex_property("bool", val=False)
 refstr = g.new_vertex_property("string", vals=(ref_str(gc[_NL]) for gc in gcs))
 ref = g.new_vertex_property("int64_t", vals=(gc[_NL] for gc in gcs))
-s = g.new_vertex_property("float", vals=(gc['survivability'] for gc in gcs))
+s = g.new_vertex_property("float", vals=(gc["survivability"] for gc in gcs))
 for gc in gcs:
     v = find_vertex(g, ref, gc[_NL])[0]
     if gc[_LEL] is not None:
@@ -82,16 +95,15 @@ for gc in gcs:
             s[nv] = 0.0
             g.add_edge(nv, v)
 
-print("Calculating initial layout...", end='', flush=True)
+print("Calculating initial layout...", end="", flush=True)
 start = time()
 count = 0
 pos = sfdp_layout(g)
-print(f'{time()-start:.2f}s')
+print(f"{time()-start:.2f}s")
 print("Creating graph widget...")
 
 
 class egp_GraphWidget(GraphWidget):
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.single_gc = False
@@ -101,6 +113,7 @@ class egp_GraphWidget(GraphWidget):
         self.set_valign(Gtk.Align.FILL)
 
     """Add a single left click event"""
+
     def button_release_event(self, widget, event):
         if event.button == 1 and not self.moved_picked:
             if not self.single_gc and isinstance(self.picked, Vertex):
@@ -117,14 +130,18 @@ class egp_GraphWidget(GraphWidget):
 
     def motion_notify_event(self, widget, event):
         state = event.window.get_pointer()[3] if event.is_hint else event.state
-        if not (state & Gdk.ModifierType.BUTTON1_MASK and
-            not state & Gdk.ModifierType.CONTROL_MASK and
-            not state & Gdk.ModifierType.SHIFT_MASK):
+        if not (
+            state & Gdk.ModifierType.BUTTON1_MASK
+            and not state & Gdk.ModifierType.CONTROL_MASK
+            and not state & Gdk.ModifierType.SHIFT_MASK
+        ):
             super().motion_notify_event(widget, event)
 
-#graph_draw(g, vcmap=inferno, vertex_fill_color=s, bg_color=(1,1,1,1), output_size=(1000,1000), output='gcs.png')
-graph = egp_GraphWidget(g, fit_view=True, pos=pos, vcmap=jet, vertex_fill_color=s,
-                            bg_color=(1,1,1,1))
+
+# graph_draw(g, vcmap=inferno, vertex_fill_color=s, bg_color=(1,1,1,1), output_size=(1000,1000), output='gcs.png')
+graph = egp_GraphWidget(
+    g, fit_view=True, pos=pos, vcmap=jet, vertex_fill_color=s, bg_color=(1, 1, 1, 1)
+)
 
 import sys
 
@@ -266,12 +283,12 @@ class AppWindow(Gtk.ApplicationWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.set_default_size(1200,1000)
+        self.set_default_size(1200, 1000)
 
         colour_mapping_action = Gio.SimpleAction.new_stateful(
             "colour_mapping",
-            GLib.VariantType.new('s'),
-            GLib.Variant.new_string('Inferno')
+            GLib.VariantType.new("s"),
+            GLib.Variant.new_string("Inferno"),
         )
         colour_mapping_action.connect("activate", self.set_colour_mapping)
         self.add_action(colour_mapping_action)
@@ -295,20 +312,21 @@ class AppWindow(Gtk.ApplicationWindow):
 
         self.separator = Gtk.HSeparator()
 
-        self.label2_text = (
-            "Reference\t\t: {0}\n"
-            "Survivability\t: {1}\n"
-        )
+        self.label2_text = "Reference\t\t: {0}\n" "Survivability\t: {1}\n"
 
         self.label2 = Gtk.Label(margin=10)
-        self.label2.set_markup(self.label2_text.format('-','-'))
+        self.label2.set_markup(self.label2_text.format("-", "-"))
         self.label2.set_justify(Gtk.Justification.LEFT)
         self.label2.set_halign(Gtk.Align.START)
 
         self.grid.attach(graph, 0, 0, 5, 9)
         self.grid.attach_next_to(self.label1, graph, Gtk.PositionType.RIGHT, 1, 4)
-        self.grid.attach_next_to(self.separator, self.label1, Gtk.PositionType.BOTTOM, 1, 1)
-        self.grid.attach_next_to(self.label2, self.separator, Gtk.PositionType.BOTTOM, 1, 4)
+        self.grid.attach_next_to(
+            self.separator, self.label1, Gtk.PositionType.BOTTOM, 1, 1
+        )
+        self.grid.attach_next_to(
+            self.label2, self.separator, Gtk.PositionType.BOTTOM, 1, 4
+        )
 
         self.add(self.grid)
         self.show_all()
@@ -319,28 +337,31 @@ class AppWindow(Gtk.ApplicationWindow):
 
     def set_colour_mapping(self, action, value):
         action.set_state(value)
-        graph.kwargs['vcmap'] = eval(value.get_string().lower())
+        graph.kwargs["vcmap"] = eval(value.get_string().lower())
         graph.regenerate_surface(reset=True)
         graph.queue_draw()
 
     def key_press_callback(self, w, g, keyval, picked, pos, vprops, eprops):
-        if keyval == ord('c'):
+        if keyval == ord("c"):
             graph.single_gc = False
             graph.g.set_vertex_filter(None)
             graph.regenerate_surface(reset=True)
             graph.queue_draw()
 
     """Add update panel information picked vertex."""
+
     def motion_notify_event(self, widget, event):
         if isinstance(graph.picked, Vertex):
-            label2_text = self.label2_text.format(refstr[graph.picked],
-                '{:0.3f}'.format(s[graph.picked]))
+            label2_text = self.label2_text.format(
+                refstr[graph.picked], "{:0.3f}".format(s[graph.picked])
+            )
             self.previous_picked = graph.picked
             self.label2.set_markup(label2_text)
         elif self.previous_picked is not None:
-            label2_text = self.label2_text.format('-','-')
+            label2_text = self.label2_text.format("-", "-")
             self.label2.set_markup(label2_text)
         # else did not change
+
 
 class Application(Gtk.Application):
     def __init__(self, *args, **kwargs):
@@ -348,7 +369,7 @@ class Application(Gtk.Application):
             *args,
             application_id="org.example.myapp",
             flags=Gio.ApplicationFlags.HANDLES_COMMAND_LINE,
-            **kwargs
+            **kwargs,
         )
         self.window = None
 
